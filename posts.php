b088bb96bf71d661fae3fcd14553faa2ae7f9971
bSolman <33519000+bSolman@users.noreply.github.com>
2018-05-28 22:55:45 +0200

    lagt admin session

--- posts.php
+++ posts.php
@@ -6,6 +6,7 @@
  * and open the template in the editor.
  */
 session_start();
+include 'include/wievs/_header.php';
 echo $_SESSION['UserName'];
 if($_SESSION['loggedIn'] === TRUE){
     echo 'logged in';
@@ -16,28 +17,6 @@ else{
 }
 
 function loadPage(){
-    echo '
-        <link href="assets/css/post.css" rel="stylesheet" type="text/css"/>
-        <body>
-        <div id="mainDiv">
-            <header>
-                <h1 id="logo">Post-it</h1>
-                <h1 id="text">Your page</h1>
-                <a href="#"><h1>Logga ut</h1></a>
-            </header>
-                <div id="mainDiv">
-                    <div id="textDiv">
-                        <h3>RunEvents</h3>
-                        <form id="sendMessage" name="sendMessage" method="post" action="posts-create.php">
-                        </form>
-                        <textarea rows="4" cols="50" name="comment" id="comment" form="sendMessage">
-                        Enter text here...</textarea>
-                        <input type="submit" form="sendMessage" value="Skicka">
-                    </div>
-                    <div id="thePosts">
-                        
-                    </div>
-                </div>
-            </div>
-        </body>';
+    echo '<link href="assets/css/post.css" rel="stylesheet" type="text/css"/>
+        <body background="assets/img/post-it.jpg"></body>';
 }
