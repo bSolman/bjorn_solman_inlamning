889e5c7ba1851663f8dac8b598f7ce48dbefd323
bSolman <33519000+bSolman@users.noreply.github.com>
2018-05-28 23:18:36 +0200

    lagt admin session

--- logout-process.php
+++ logout-process.php
@@ -1,8 +1,8 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    session_start();
+    unset($_SESSION['loggedIn']);
+    unset($_SESSION['UserName']);
+    unset($_SESSION['email']);
+    session_destroy();
+    header('Location: index.php');
 
