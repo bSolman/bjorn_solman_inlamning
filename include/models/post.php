f25d94f72cbe1eb6e0b16530c6570deef7559078
bSolman <33519000+bSolman@users.noreply.github.com>
2018-05-29 17:18:08 +0200

    footer edit

--- include/models/post.php
+++ include/models/post.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include '../../dbConnect.php';
 
-/**
- * Description of post
- *
- * @author Björn Solman
- */
-class post {
-    //put your code here
-}
+
